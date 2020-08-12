effc6bfaa2dfa5090d3722b8c069e4d217063773
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-08-11 23:22:05 -0400

    clean up

--- example(not updated)/index.tsx
+++ example(not updated)/index.tsx
@@ -1,14 +1,9 @@
 import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Thing } from "../dist";
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
+  return <div>Not available</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
